1bbc08c22cfe65ddb1f08e80c9ff489bf4e1a639
Grace Lee <igracelee1@gmail.com>
2019-06-22 23:53:27 -0400

    VideoDetail description

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,13 @@ const VideoDetail = (props) => {
     if(!props.video) {
         return <div>Loading...</div>
     }
-    return <div>{props.video.snippet.title}</div>
+
+    return (
+        <div className="ui segment">
+            <h4 className="ui header">{props.video.snippet.title}</h4>
+            <p>{props.video.snippet.description}</p>
+        </div>
+    );
 }
 
 export default VideoDetail;
